47b70b9fbee87442d0e77d95bd9ad49c91d7fdcd
StarMac <starmac2008@gmail.com>
2023-05-16 22:18:22 +0300

    Refactored AuthorizationActivity & AuthorizationViewModel

--- app/src/main/java/com/example/pizzeriaapp/view/activity/AuthorizationActivity.kt
+++ app/src/main/java/com/example/pizzeriaapp/view/activity/AuthorizationActivity.kt
@@ -1,43 +1,31 @@
 package com.example.pizzeriaapp.view.activity
 
-import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
-import android.util.Log
 import android.view.View
-import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.example.pizzeriaapp.databinding.ActivityAuthorizationBinding
-import com.example.pizzeriaapp.model.Order
-import com.example.pizzeriaapp.model.Pizza
-import com.example.pizzeriaapp.model.User
+import com.example.pizzeriaapp.viewmodel.AuthorizationViewModel
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-import java.text.SimpleDateFormat
-import java.util.*
 
 class AuthorizationActivity :
     BaseActivity<ActivityAuthorizationBinding>(ActivityAuthorizationBinding::inflate) {
     private lateinit var googleSignInClient: GoogleSignInClient
     private lateinit var auth: FirebaseAuth
-    //TODO delete test
-    private lateinit var userDatabaseRef : DatabaseReference
-    private lateinit var pizzaDatabaseRef : DatabaseReference
-    private lateinit var orderDatabaseRef: DatabaseReference
-
+    private lateinit var viewModel: AuthorizationViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         init()
+        setupViewModel()
+        setupObservers()
     }
 
     private fun init() {
@@ -45,24 +33,43 @@ class AuthorizationActivity :
         binding.signUpTextButton.setOnClickListener { showSignUp() }
         binding.signInButton.setOnClickListener { signInWithEmailCheck() }
         binding.signUpButton.setOnClickListener { signUpWithEmailCheck() }
-        binding.googleImageButton.setOnClickListener {
-            signInWithGoogle()
-        }
+        binding.googleImageButton.setOnClickListener { signInWithGoogle() }
 
         auth = Firebase.auth
-        //TODO delete test
-        userDatabaseRef = Firebase.database.getReference("User")
-        pizzaDatabaseRef = Firebase.database.getReference("Pizza")
-        orderDatabaseRef = Firebase.database.getReference("Order")
 
-
-        //Configure Google SignIn
+        // Configure Google SignIn
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken("858280102305-4fdri7uf50q41skc4t017c5k9p51iuqq.apps.googleusercontent.com")
             .requestEmail()
             .build()
 
         googleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
+
+    private fun setupViewModel() {
+        viewModel = ViewModelProvider(this)[AuthorizationViewModel::class.java]
+    }
+
+    private fun setupObservers() {
+        viewModel.googleSignInResult.observe(this) { isSuccess ->
+            if (isSuccess) {
+                // Вход с помощью Google успешен, обновите пользовательский интерфейс
+                val user = auth.currentUser
+                updateUI(user)
+            } else {
+                // Вход с помощью Google не удался, обработайте ошибку
+                updateUI(null)
+            }
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            viewModel.handleGoogleSignInResult(data)
+        }
     }
 
     private fun signUpWithEmailCheck() {
@@ -76,8 +83,9 @@ class AuthorizationActivity :
             binding.confirmPasswordEditText.error = "Passwords don't match"
             binding.confirmPasswordEditText.requestFocus()
         } else {
-            signUpWithEmail(binding.emailEditText.text.toString().trim(),
-                binding.passwordEditText.text.toString().trim())
+            val email = binding.emailEditText.text.toString().trim()
+            val password = binding.passwordEditText.text.toString().trim()
+            viewModel.signUpWithEmail(email, password)
         }
     }
 
@@ -89,50 +97,13 @@ class AuthorizationActivity :
             binding.passwordEditText.error = "Password cannot be empty"
             binding.passwordEditText.requestFocus()
         } else {
-            signInWithEmail(binding.emailEditText.text.toString().trim(),
-                binding.passwordEditText.text.toString().trim())
+            val email = binding.emailEditText.text.toString().trim()
+            val password = binding.passwordEditText.text.toString().trim()
+            viewModel.signInWithEmail(email, password)
         }
     }
 
-    private fun signUpWithEmail(email : String, password : String) {
-        auth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "createUserWithEmail:success")
-                    val user = auth.currentUser
-                    //TODO delete test
-                    val newUser = User(user!!.email)
-                    userDatabaseRef.child(user.uid).setValue(newUser)
-                    updateUI(user)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
-                    updateUI(null)
-                }
-            }
-    }
-
-    private fun signInWithEmail(email : String, password : String) {
-        auth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "signInWithEmail:success")
-                    val user = auth.currentUser
-                    updateUI(user)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInWithEmail:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
-                    updateUI(null)
-                }
-            }
-    }
-
+    @Suppress("DEPRECATION")
     private fun signInWithGoogle() {
         val signInIntent = googleSignInClient.signInIntent
         startActivityForResult(signInIntent, RC_SIGN_IN)
@@ -144,50 +115,6 @@ class AuthorizationActivity :
         updateUI(currentUser)
     }
 
-    //TODO Refactor to not deprecated
-    @Suppress("DEPRECATION")
-    @Deprecated("Deprecated in Java")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        //Result returned from launching the intent from GoogleSighInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-                //Google Sign In was successful, authenticate with Firebase
-                val account = task.getResult(ApiException::class.java)!!
-                Log.d(TAG, "firebaseAuthWithGoogle" + account.id)
-                firebaseAuthWithGoogle(account.idToken!!)
-            } catch (e: ApiException) {
-                //Google Sign In failed, update UI appropriately
-                Log.d(TAG, "Google sign in failed", e)
-            }
-        }
-    }
-
-    private fun firebaseAuthWithGoogle(idToken: String) {
-        val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
-        auth.signInWithCredential(firebaseCredential)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "signInWithCredential:success")
-                    val user = auth.currentUser
-                    //TODO delete test
-                    val newUser = User(user!!.email)
-                    userDatabaseRef.child(user.uid).setValue(newUser)
-
-                    //TODO delete test 2
-//                    testFirebase(user)
-                    updateUI(user)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInWithCredential:failure", task.exception)
-                    updateUI(null)
-                }
-            }
-    }
-
     private fun updateUI(user: FirebaseUser?) {
         if (user != null) {
             val intent = Intent(applicationContext, MainActivity::class.java)
@@ -195,27 +122,6 @@ class AuthorizationActivity :
             startActivity(intent)
         }
     }
-
-        private fun testFirebase(user: FirebaseUser?){
-            if (user != null){
-                val pizza1 = Pizza("Hawaiian Pizza", "25",
-                    "https://www.jessicagavin.com/wp-content/uploads/2020/07/hawaiian-pizza-16-1200.jpg",
-                    "mozzarella, pineapple, bacon, oregano, ounces, pizza sauce")
-
-                val pizza2 = Pizza("Pizza With Tomatoes", "21",
-                    "https://eclecticrecipes.com/wp-content/uploads/2011/10/zucchini-pizza.jpg",
-                    "mozzarella, tomatoes, bacon, oregano, ounces, pizza sauce")
-                val sdf = SimpleDateFormat("dd MM yyyy HH:mm:ss")
-                val calendar = Calendar.getInstance()
-                val orderDate = sdf.format(calendar.time)
-                val order1 = Order(orderDate,pizza1.name,pizza1.photo,"Status: Waiting",pizza1.price + " uah",user.uid)
-                val order2 = Order(orderDate,pizza2.name,pizza2.photo,"Status: Waiting",pizza2.price + " uah",user.uid)
-                pizzaDatabaseRef.child(pizza1.name!!).setValue(pizza1)
-                pizzaDatabaseRef.child(pizza2.name!!).setValue(pizza2)
-                orderDatabaseRef.child(order1.time + " " + order1.uid).setValue(order1)
-            }
-        }
-
     private fun showSignIn() {
         binding.emailEditText.setText("")
         binding.passwordEditText.setText("")
@@ -248,5 +154,4 @@ class AuthorizationActivity :
         const val RC_SIGN_IN = 1001
         const val EXTRA_NAME = "EXTRA NAME"
     }
-
 }
--- app/src/main/java/com/example/pizzeriaapp/viewmodel/AuthorizationViewModel.kt
+++ app/src/main/java/com/example/pizzeriaapp/viewmodel/AuthorizationViewModel.kt
@@ -1,7 +1,78 @@
 package com.example.pizzeriaapp.viewmodel
 
 import android.app.Application
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.pizzeriaapp.model.User
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class AuthorizationViewModel (application: Application) : AndroidViewModel(application) {
+    private val auth: FirebaseAuth = Firebase.auth
+    private val userDatabaseRef: DatabaseReference = Firebase.database.getReference("User")
+
+    private val _googleSignInResult = MutableLiveData<Boolean>()
+    val googleSignInResult: LiveData<Boolean> get() = _googleSignInResult
+
+    fun signUpWithEmail(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    val newUser = User(user!!.email)
+                    userDatabaseRef.child(user.uid).setValue(newUser)
+                    _googleSignInResult.value = true
+                } else {
+                    _googleSignInResult.value = false
+                }
+            }
+    }
+
+    fun signInWithEmail(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    _googleSignInResult.value = true
+                } else {
+                    _googleSignInResult.value = false
+                    Toast.makeText(getApplication(), "Incorrect login or password", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun signInWithGoogle(idToken: String) {
+        val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(firebaseCredential)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    val newUser = User(user!!.email)
+                    userDatabaseRef.child(user.uid).setValue(newUser)
+                    _googleSignInResult.value = true
+                } else {
+                    _googleSignInResult.value = false
+                }
+            }
+    }
+    fun handleGoogleSignInResult(data: Intent?) {
+        val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+        try {
+            val account = task.getResult(ApiException::class.java)!!
+            signInWithGoogle(account.idToken!!)
+        } catch (e: ApiException) {
+            _googleSignInResult.value = false
+            Log.d(TAG, "Google sign in failed", e)
+        }
+    }
 }
